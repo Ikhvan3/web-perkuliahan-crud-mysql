69abd49d6973d94c9ebdff940ae7691f3d29eb7a
Ikhvan <ikhvankanaya123@gmail.com>
2024-09-30 01:18:55 +0700

    perubahan proses ganti foto  - mhs

--- editMhs.php
+++ editMhs.php
@@ -1,58 +1,67 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Slamet Ikhvan Nurhana Rifki - A12.2022.06882</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Bootstrap lokal -->
+    <title>Slamet Ikhvan Nurhana Rifki - A12.2022.06882</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap-5.1.3/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/styleku.css">
-	<!-- script src="bootstrap4/jquery/3.3.1/jquery-3.3.1.js"></script -->
-	<!-- script src="bootstrap4/js/bootstrap.js"></script -->
-	<script src="bootstrap-5.1.3/jquery/3.3.1/jquery-3.3.1.js"></script>
-	<script src="bootstrap-5.1.3/dist/js/bootstrap.js"></script>
-
+    <link rel="stylesheet" type="text/css" href="css/styleku.css">
+    <script src="bootstrap-5.1.3/jquery/3.3.1/jquery-3.3.1.js"></script>
+    <script src="bootstrap-5.1.3/dist/js/bootstrap.js"></script>
 </head>
 <body>
-	<?php
-	require "fungsi.php";
-	require "head.html";
-	$id=$_GET['kode'];
-	$sql="select * from mhs where id='$id'";
-	$qry=mysqli_query($koneksi,$sql);
-	$row=mysqli_fetch_assoc($qry);
-	?>
-	<div class="utama">
-		<h2 class="mb-3 text-center">EDIT DATA MAHASISWA</h2>	
-		<div class="row">
-		<div class="col-sm-3 text-center">
-			<img class="rounded img-thumbnail"src="foto/<?php echo $row['foto']?>">
-			<div>
-				[ <a href="gantiFotoMhs.php?id=<?php echo $row['id']?>">Ganti Foto</a> ]
-			</div>	
-		</div>
-		<div class="col-sm-9">
-			<form enctype="multipart/form-data" method="post" action="sv_editMhs.php">
-				<div class="form-group">
-					<label for="nim">NIM:</label>
-					<input class="form-control" type="text" name="nim" id="nim" value="<?php echo $row['nim']?>" readonly>
-				</div>
-				<div class="form-group">
-					<label for="nama">Nama:</label>
-					<input class="form-control" type="text" name="nama" id="nama" value="<?php echo $row['nama']?>">
-				</div>
-				<div class="form-group">
-					<label for="email">Email:</label>
-					<input class="form-control" type="email" name="email" id="email" value="<?php echo $row['email']?>">
-				</div>				
-				<div>		
-					<button class="btn btn-primary" type="submit" id="submit">Simpan</button>
-				</div>
-				<input type="hidden" name="id" id="id" value="<?php echo $id?>">
-			</form>
-		</div>
-		</div>
-	</div>
-	
+    <?php
+    require "fungsi.php";
+    require "head.html";
+    $id = $_GET['kode'];
+    $sql = "SELECT * FROM mhs WHERE id='$id'";
+    $qry = mysqli_query($koneksi, $sql);
+    $row = mysqli_fetch_assoc($qry);
+    ?>
+    <div class="utama">
+        <h2 class="mb-3 text-center">EDIT DATA MAHASISWA</h2>    
+        <div class="row">
+            <div class="col-sm-3 text-center">
+                <img class="rounded img-thumbnail" src="foto/<?php echo $row['foto']?>" id="preview">
+            </div>
+            <div class="col-sm-9">
+                <form enctype="multipart/form-data" method="post" action="sv_editMhs.php">
+                    <div class="form-group">
+                        <label for="nim">NIM:</label>
+                        <input class="form-control" type="text" name="nim" id="nim" value="<?php echo $row['nim']?>" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama">Nama:</label>
+                        <input class="form-control" type="text" name="nama" id="nama" value="<?php echo $row['nama']?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input class="form-control" type="email" name="email" id="email" value="<?php echo $row['email']?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="foto">Ganti Foto:</label>
+                        <input class="form-control" type="file" name="foto" id="foto" onchange="previewImage(this);">
+                    </div>
+                    <div class="mt-3">        
+                        <button class="btn btn-primary" type="submit" id="submit">Simpan</button>
+                    </div>
+                    <input type="hidden" name="id" id="id" value="<?php echo $id?>">
+                    <input type="hidden" name="foto_lama" value="<?php echo $row['foto']?>">
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+    function previewImage(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#preview').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    </script>
 </body>
 </html>
--- sv_editMhs.php
+++ sv_editMhs.php
@@ -1,17 +1,49 @@
 <?php
-
 require "fungsi.php";
 
+$id = $_POST["id"];
+$nama = $_POST["nama"];
+$email = $_POST["email"];
+$foto_lama = $_POST["foto_lama"];
+$uploadOk = 1;
 
-$id=$_POST["id"];
-$nama=$_POST["nama"];
-$email=$_POST["email"];
-$uploadOk=1;
+// Proses upload foto baru jika ada
+if(isset($_FILES['foto']) && $_FILES['foto']['error'] == 0) {
+    $folder_upload = "foto/";
+    $nama_file = basename($_FILES['foto']['name']);
+    $target_file = $folder_upload . $nama_file;
+    $jenis_file = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    
+    // Cek jenis file
+    if($jenis_file != "jpg" && $jenis_file != "png" && $jenis_file != "jpeg" && $jenis_file != "gif") {
+        echo "Maaf, hanya file JPG, JPEG, PNG & GIF yang diperbolehkan.";
+        $uploadOk = 0;
+    }
+    
+    // Upload file
+    if ($uploadOk == 1) {
+        if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
+            // Hapus foto lama jika ada
+            if (!empty($foto_lama) && file_exists("foto/" . $foto_lama)) {
+                unlink("foto/" . $foto_lama);
+            }
+            $foto = $nama_file;
+        } else {
+            echo "Maaf, terjadi kesalahan saat mengupload file.";
+            $uploadOk = 0;
+        }
+    }
+} else {
+    $foto = $foto_lama; // Jika tidak ada foto baru diupload, gunakan foto lama
+}
 
-//membuat query
-$sql="update mhs set nama='$nama',
-					 email='$email'
-					 where id='$id'";
-mysqli_query($koneksi,$sql) or die(mysqli_error($koneksi));
-header("location:ajaxUpdateMhs.php");
+if ($uploadOk == 1) {
+    // Update data mahasiswa
+    $sql = "UPDATE mhs SET nama='$nama', email='$email', foto='$foto' WHERE id='$id'";
+    if(mysqli_query($koneksi, $sql)) {
+        header("location:ajaxUpdateMhs.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+    }
+}
 ?>
